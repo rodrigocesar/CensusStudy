c9f36ae8ed1fe031459f256781fc989da2d1d808
rodrigocesar <rodrigocesar.jo@gmail.com>
2014-05-29 15:45:32 -0300

    Adicionada funcao principal para tratamento e analise dos dados.

---
+++ Main.R
@@ -0,0 +1,34 @@
+## Script principal 
+
+
+######################################################################
+############ Carregar funcoes definidas pelo usuario #################
+######################################################################
+
+## funcao para baixar os dados da amostra do site do IBGE
+source('DownloadData.R') 
+
+source('RemovePunct.R')
+## funcao para ler as tabelas
+source('ReadBigTable.R') 
+## funcao para extrair os dados por municipio
+source('ExtractData.R') 
+
+######################################################################
+#################### Declarar variaveis globais ######################
+######################################################################
+
+citycode <<- '49904'
+
+
+######################################################################
+######### Fazer Download dos dados da amostra do Censo 2010 ##########
+######################################################################
+
+DownloadData()
+
+######################################################################
+############## Extrair os dados de Sao Jose dos Campos ###############
+######################################################################
+
+data = ExtractData(citycode = citycode)
